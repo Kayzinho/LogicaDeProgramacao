ebaf182cd59a5cc1395defef643300be9270d812
Kay <zetec-31@hotmail.com>
2025-03-06 17:40:34 -0300

    first commit

---
+++ LógicaDeProgramaçãoATV/code.py
@@ -0,0 +1,14 @@
+import math
+
+def calcular(altura, largura):
+    try:
+        altura = float(altura)
+        largura = float(largura)
+
+        area = altura * largura
+        litros = area/2
+
+        return f"Área: {area:.2f} m²\nVocê precisará de aproximadamente {math.ceil(litros)} litros de tinta."
+    
+    except ValueError:
+        return 'Valores inválidos'
---
+++ LógicaDeProgramaçãoATV/window.py
@@ -0,0 +1,25 @@
+import FreeSimpleGUI as sg
+from code import calcular
+
+layout = [
+    [sg.Text('Calcular a quantidade de tinta necessária por m²')],
+    [sg.Text('Altura:'), sg.InputText(key='Altura')],
+    [sg.Text('Largura:'), sg.InputText(key='Largura')],
+    [sg.Button('Calcular'), sg.Button('Sair')],
+    [sg.Text('', key='Area')],
+]
+
+janela = sg.Window('Cálculo de tinta por m²', layout)
+
+while True:
+    evento, valores = janela.read()
+
+    if evento == sg.WIN_CLOSED or evento == 'Sair':
+        break
+
+    if evento == 'Calcular':
+        resultado = calcular(valores['Altura'], valores['Largura'])
+        
+        janela['Area'].update(resultado)
+
+janela.close()
